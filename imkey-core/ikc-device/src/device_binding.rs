2923ebba90bd8f894c6a715f722e26c45679358c
xiaoguang <xiaoguang101010@163.com>
2023-03-15 11:25:05 +0800

    feat: pr template test

--- imkey-core/ikc-device/src/device_binding.rs
+++ imkey-core/ikc-device/src/device_binding.rs
@@ -241,9 +241,9 @@ pub fn bind_test() {
 }
 
 // pub const TEST_KEY_PATH: &str = "/tmp/";
-// pub const TEST_BIND_CODE: &str = "MCYNK5AH";
+pub const TEST_BIND_CODE: &str = "MCYNK5AH";
 pub const TEST_KEY_PATH: &str = "/tmp/";
-pub const TEST_BIND_CODE: &str = "DJKP4NUR";
+// pub const TEST_BIND_CODE: &str = "DJKP4NUR";
 
 #[cfg(test)]
 mod test {
