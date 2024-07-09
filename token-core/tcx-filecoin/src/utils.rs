1e84ca88c01feff6823607488ccb6de0431cd7d9
XuNeal <imxuneal@gmail.com>
2024-07-09 16:32:58 +0800

    fix: remove deprecated fil library

--- token-core/tcx-filecoin/src/utils.rs
+++ token-core/tcx-filecoin/src/utils.rs
@@ -1,5 +1,7 @@
 use blake2b_rs::Blake2bBuilder;
-
+use cid::Cid;
+use fvm_ipld_encoding::Error;
+use multihash_codetable::{Code, MultihashDigest};
 pub enum HashSize {
     Checksum = 4,
     Payload = 20,
@@ -16,6 +18,25 @@ pub fn digest(ingest: &[u8], hash_size: HashSize) -> Vec<u8> {
     hasher.finalize(&mut result);
     result[0..size].to_vec()
 }
+
+/// Extension methods for constructing `dag-cbor` [Cid]
+pub trait CidCborExt {
+    /// Default CID builder for Filecoin
+    ///
+    /// - The default codec is [`fvm_ipld_encoding::DAG_CBOR`]
+    /// - The default hash function is 256 bit BLAKE2b
+    ///
+    /// This matches [`abi.CidBuilder`](https://github.com/filecoin-project/go-state-types/blob/master/abi/cid.go#L49) in go
+    fn from_cbor_blake2b256<S: serde::ser::Serialize>(obj: &S) -> Result<Cid, Error> {
+        let bytes = fvm_ipld_encoding::to_vec(obj)?;
+        Ok(Cid::new_v1(
+            fvm_ipld_encoding::DAG_CBOR,
+            Code::Blake2b256.digest(&bytes),
+        ))
+    }
+}
+
+impl CidCborExt for Cid {}
 
 #[cfg(test)]
 mod tests {
