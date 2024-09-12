7b1931bf882daa42177cc46f1c76227bc92926cc
XuNeal <imxuneal@gmail.com>
2024-09-12 15:18:40 +0800

    fix: webusb build error

--- imkey-core/ikc-webusb/src/lib.rs
+++ imkey-core/ikc-webusb/src/lib.rs
@@ -1,21 +1,13 @@
 extern crate wasm_bindgen;
 extern crate web_sys;
 use wasm_bindgen::prelude::*;
-use web_sys::{ Navigator, Window, Usb, UsbDevice};
 use web_sys::window;
-
-
+use web_sys::{Navigator, Usb, UsbDevice, Window};
 
 pub fn test_a() {
     let navigator = window().expect("window should be available").navigator();
-    if let Some(usb) = navigator.usb() {
-        // 使用 WebUSB API
-    } else {
-        // console::log_1(&JsValue::from_str("WebUSB not supported"));
-    }
+    let usb = navigator.usb();
 }
 
 #[cfg(test)]
-mod test{
-
-}
+mod test {}
