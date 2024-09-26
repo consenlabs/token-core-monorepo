3ff55580bb3afc5a7aecd2375a8960a8af223c2c
XuNeal <imxuneal@gmail.com>
2024-09-27 05:48:03 +0800

    feat: update rusttoolchain and deps

--- token-core/tcx-ton/src/signer.rs
+++ token-core/tcx-ton/src/signer.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use std::sync::Arc;
 use std::time::SystemTime;
 
@@ -25,13 +26,17 @@ impl TraitTransactionSigner<TonTxIn, TonTxOut> for Keystore {
             secret_key: vec![],
         };
 
+        let wallet = TonWallet::derive_default(WalletVersion::V4R2, &null_key_pair)?;
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_secs() as u32;
         let transfer = if tx.is_jetton {
-            let jetton_amount = BigUint::from(&tx.jetton_amount);
+            let jetton_amount = BigUint::from_str(&tx.jetton_amount)?;
             let from = tx.from.parse()?;
             let to = tx.to.parse()?;
-            let amount = BigUint::from(&tx.amount);
-            let mut jetton_transfer =
-                JettonTransferMessage::new(&to, &jetton_amount).with_response_destination(&from);
+            let amount = BigUint::from_str(&tx.amount)?;
+            let mut jetton_transfer = JettonTransferMessage::new(&to, &jetton_amount);
+            jetton_transfer.with_response_destination(&from);
             jetton_transfer.set_query_id(tx.query_id);
             let cell = jetton_transfer.build()?;
 
@@ -60,20 +65,16 @@ impl TraitTransactionSigner<TonTxIn, TonTxOut> for Keystore {
             // })
         } else {
             let to = tx.to.parse()?;
-            let amount = BigUint::from(&tx.amount);
+            let amount = BigUint::from_str(&tx.amount)?;
             TransferMessage::new(CommonMsgInfo::new_default_internal(&to, &amount))
                 // .with_data(Arc::new(cell))
                 .build()?
         };
 
-        let wallet = TonWallet::derive_default(WalletVersion::V4R2, &null_key_pair)?;
-        let now = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)?
-            .as_secs() as u32;
         let body = wallet.create_external_body(
             now + 60,
             tx.sequence_no.try_into().unwrap(),
-            vec![transfer],
+            vec![Arc::new(transfer)],
         )?;
         let hash = body.cell_hash();
         let sig = self.ed25519_sign(&hash.to_vec(), &params.derivation_path)?;
@@ -93,15 +94,20 @@ impl TraitTransactionSigner<TonTxIn, TonTxOut> for Keystore {
 #[cfg(test)]
 mod test_super {
     use crate::transaction::TonTxIn;
-    use tcx_constants::{CoinInfo, CurveType};
-    use tcx_keystore::{HdKeystore, Keystore, Metadata};
+    use tcx_common::ToHex;
+    use tcx_constants::{CoinInfo, CurveType, TEST_MNEMONIC, TEST_PASSWORD};
+    use tcx_keystore::{HdKeystore, Keystore, Metadata, SignatureParameters, TransactionSigner};
+    use tonlib_core::wallet::WalletVersion;
 
     #[test]
     fn test_nacl_sign() {
-        let hd = HdKeystore::new("imtokenq", Metadata::default());
+        let hd =
+            HdKeystore::from_mnemonic(TEST_MNEMONIC, TEST_PASSWORD, Metadata::default()).unwrap();
+        let path = "m/44'/607'/0'/0'/0'".to_string();
         let coin = CoinInfo {
             coin: "TON".to_string(),
-            derivation_path: "m/44'/607'/0'/0/0".to_string(),
+            // todo: the ton path is not official
+            derivation_path: "m/44'/607'/0'/0'/0'".to_string(),
             curve: CurveType::ED25519,
             network: "".to_string(),
             seg_wit: "".to_string(),
@@ -109,22 +115,51 @@ mod test_super {
         };
 
         let mut keystore = Keystore::Hd(hd);
-        keystore.unlock_by_password("imtokenq").unwrap();
+        keystore.unlock_by_password(TEST_PASSWORD).unwrap();
         let acc = keystore
             .derive_coin::<crate::address::TonAddress>(&coin)
             .unwrap();
-        assert_eq!("", acc.address);
+        assert_eq!(
+            "EQCKJfmBlnFiINcL1MoCjuyxULXaOEA-k5iHcr4L18RuhQHo",
+            acc.address
+        );
+        let sec_key = keystore.get_private_key(CurveType::ED25519, &path).unwrap();
+        assert_eq!(
+            sec_key.to_bytes().to_hex(),
+            "34815c96ad2434988d86a01e4b639acf41e8ecac7eeb260635b8a47028bbefd3"
+        );
 
-        // let tx_in = TonTxIn {
-        //     from: "".to_string(),
-        //     to: "".to_string(),
-        //     amount: "".to_string(),
-        //     memo: "".to_string(),
-        //     is_jetton: false,
-        //     jetton_amount: "".to_string(),
-        //     query_id: 0,
-        //     sequence_no: 0,
-        // }
+        let tx_in = TonTxIn {
+            from: "EQCKJfmBlnFiINcL1MoCjuyxULXaOEA-k5iHcr4L18RuhQHo".to_string(),
+            to: "0QBhzrMl_WXpLg6QQDVXAaCJAiCCDczkgmIxCfBejgH4RfFK".to_string(),
+            amount: "100000000000".to_string(),
+            memo: "".to_string(),
+            is_jetton: false,
+            jetton_amount: "20000000000".to_string(),
+            query_id: 30000,
+            sequence_no: 0,
+            wallet_version: "".to_string(),
+        };
+
+        let param = SignatureParameters {
+            curve: CurveType::ED25519,
+            derivation_path: path.to_string(),
+            chain_type: "TON".to_string(),
+            network: "".to_string(),
+            seg_wit: "".to_string(),
+        };
+
+        let sign_ret = keystore.sign_transaction(&param, &tx_in).unwrap();
+        assert_eq!(sign_ret.signature, "");
+
+        let keypair = tonlib_core::mnemonic::KeyPair {
+            public_key: vec![],
+            secret_key: sec_key.to_bytes(),
+        };
+
+        let wallet =
+            tonlib_core::wallet::TonWallet::derive_default(WalletVersion::V4R2, &keypair).unwrap();
+
         //
         //
         //
