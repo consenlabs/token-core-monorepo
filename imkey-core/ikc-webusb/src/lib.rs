bae3eacbb1f594b25970a9e9670b96c4970b3bc0
xiaoguang <xiaoguang101010@163.com>
2024-09-26 17:04:44 +0800

    chore: code optimization

--- imkey-core/ikc-webusb/src/lib.rs
+++ imkey-core/ikc-webusb/src/lib.rs
@@ -1,127 +1,21 @@
-mod webusb;
-extern crate wasm_bindgen;
 extern crate web_sys;
 use wasm_bindgen::prelude::*;
-use web_sys::window;
-use web_sys::{Navigator, Usb, UsbDevice, UsbDeviceRequestOptions, UsbConfiguration, UsbInterface};
-use js_sys::Promise;
-use wasm_bindgen_futures::JsFuture;
-use serde_json::Value;
+#[cfg(target_arch = "wasm32")]
+mod webusb;
 use web_sys::console;
-use serde_wasm_bindgen::to_value;
-use parking_lot::Mutex;
-#[macro_use]
-extern crate lazy_static;
-use js_sys::Array;
-// use core::result;
-// pub type Result<T> = result::Result<T, anyhow::Error>;
-use webusb::send_and_receive;
+use core::result;
+pub type Result<T> = result::Result<T, anyhow::Error>;
 
-lazy_static! {
-    // 使用 Arc<Mutex<...>> 来包装 UsbDevice
-    pub static ref WEB_USB_DEVICE: Mutex<Option<UsbDeviceBox>> = Mutex::new(None);
-}
-#[derive(Debug)]
-pub struct UsbDeviceBox(UsbDevice);
-unsafe impl Send for UsbDeviceBox {}
-
-// #[cfg(any(target_os = "macos", target_os = "windows", target_os = "linux"))]
-// lazy_static! {
-//     pub static ref WEB_USB_DEVICE: Mutex<Vec<UsbDevice>> = Mutex::new(vec![]);
-// }
 
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen]
-pub async fn connect() -> Result<(), JsValue> {
-    let navigator = window().expect("window should be available").navigator();
-    let usb: Usb = navigator.usb();
-
-    // 手动构造过滤器对象
-    let filters = vec![serde_json::json!({
-        "vendorId": 0x096e,
-        "productId": 0x0891,
-    })];
-    // 创建 UsbDeviceRequestOptions
-    let options = UsbDeviceRequestOptions::new(&to_value(&vec![filters]).unwrap());
-    // 请求设备
-    let promise = usb.request_device(&options);
-    // 处理请求设备的结果
-    let device = wasm_bindgen_futures::JsFuture::from(promise).await;
-    // 请求设备
-    match device {
-        Ok(device) => {
-            console::log_1(&"Device requested successfully".into());
-            // 继续处理设备
-            let device: UsbDevice = device.unchecked_into();
-            web_sys::console::log_1(&format!("++++++++++++Product Name: {:?}", device.product_name()).into());
-            let open_promise = device.open();
-            JsFuture::from(open_promise).await?;
-
-            let configurations: Array = device.configurations();
-            let configuration: UsbConfiguration = configurations.get(0).unchecked_into();
-             // 获取接口数组
-             let interfaces: Array = configuration.interfaces();
-             // 获取第一个接口
-             let interface: UsbInterface = interfaces.get(0).unchecked_into();
-             let interface_number = interface.interface_number(); // 获取接口编号
-             // 选择配置，通常为默认配置 1
-            let select_configuration_promise = device.select_configuration(1);
-            JsFuture::from(select_configuration_promise).await?;
-             // 声明接口 
-            let claim_promise = device.claim_interface(interface_number);
-            wasm_bindgen_futures::JsFuture::from(claim_promise).await?;
-
-            let mut hid_device_obj = WEB_USB_DEVICE.lock();
-            *hid_device_obj = Some(UsbDeviceBox(device));
-            Ok(())
-        },
-        Err(err) => {
-            console::log_1(&format!("Error requesting device: {:?}", err).into());
-            Ok(())
-        }
-    }
-
+pub async fn connect() {
+    webusb::connect().await;
 }
 
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen]
-pub async fn send_apdu(apdu: &str) -> Result<String, JsValue> {
-
-    console::log_1(&"enter send_apdu function".into());
-    console::log_1(&format!("-->{:?}", apdu).into());
-    // 访问存储的 UsbDevice
-    let hid_device_obj = WEB_USB_DEVICE.lock();
-    let mut response_data = "".to_string();
-    if let Some(device) = &*hid_device_obj {
-        // 这里可以使用 device 进行操作
-        console::log_1(&format!("Using device: {:?}", device.0.product_name()).into());
-        response_data = send_and_receive(&device.0, hex::decode(apdu).unwrap().as_slice()).await;
-    } else {
-        console::log_1(&"No device found".into());
-    }
-    console::log_1(&format!("<--{:?}", response_data.clone()).into());
-
-    Ok(response_data)
+pub async fn send_apdu(apdu: &str) ->String {
+    webusb::send_apdu(apdu).await.unwrap()
 }
 
-#[cfg(not(target_arch = "wasm32"))]
-pub fn connect() {
-    println!("WebUSB functionality is not available in non-WASM environments");
-}
-
-#[cfg(test)]
-wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
-
-#[cfg(test)]
-mod test {
-    
-    use super::*;
-    use wasm_bindgen_test::*;
-    
-    #[wasm_bindgen_test]  
-    async fn test_connect() {
-        println!("开始测试 connect 函数");
-        super::connect().await;
-        println!("完成测试");
-    }
-}
--- imkey-core/ikc-webusb/src/webusb.rs
+++ imkey-core/ikc-webusb/src/webusb.rs
@@ -1,13 +1,16 @@
-use wasm_bindgen::JsValue;
+extern crate wasm_bindgen;
+extern crate web_sys;
+use wasm_bindgen::prelude::*;
+use web_sys::window;
 use web_sys::{Navigator, Usb, UsbDevice, UsbDeviceRequestOptions, UsbConfiguration, UsbInterface, UsbInTransferResult};
 use bytes::{BufMut, BytesMut};
 use web_sys::console;
-use super::WEB_USB_DEVICE;
-use crate::UsbDeviceBox;
-use js_sys::{Uint8Array, DataView, Promise, ArrayBuffer}; 
-// use crate::Result;
+use js_sys::{Uint8Array, DataView, Promise, ArrayBuffer, Array}; 
 use wasm_bindgen_futures::JsFuture;
-use crate::wasm_bindgen::JsCast;
+use parking_lot::Mutex;
+use serde_wasm_bindgen::to_value;
+use lazy_static::lazy_static;
+use crate::Result;
 
 const COMMAND_TYPE_MESSAGE: u8 = 0x43 | 0x80;
 const COMMAND_TYPE_CANCEL: u8 = 0x51 | 0x80;
@@ -23,8 +26,87 @@ const ERR_DEVICE_BUSY: u8 = 0x06;
 const ERR_DEVICE_CANCEL: u8 = 0xFE;
 const ERR_OTHER: u8 = 0x07;
 
-pub fn webusb_send(apdu: &str, timeout: i32) -> String {
-    "".to_string()
+
+
+lazy_static! {
+    pub static ref WEB_USB_DEVICE: Mutex<Option<UsbDeviceBox>> = Mutex::new(None);
+}
+#[derive(Debug)]
+pub struct UsbDeviceBox(UsbDevice);
+unsafe impl Send for UsbDeviceBox {}
+
+#[cfg(target_arch = "wasm32")]
+// #[wasm_bindgen]
+pub async fn connect() -> Result<()> {
+    let navigator = window().expect("window should be available").navigator();
+    let usb: Usb = navigator.usb();
+
+    // 手动构造过滤器对象
+    let filters = vec![serde_json::json!({
+        "vendorId": 0x096e,
+        "productId": 0x0891,
+    })];
+    // 创建 UsbDeviceRequestOptions
+    let options = UsbDeviceRequestOptions::new(&to_value(&vec![filters]).unwrap());
+    // 请求设备
+    let promise = usb.request_device(&options);
+    // 处理请求设备的结果
+    let device = wasm_bindgen_futures::JsFuture::from(promise).await;
+    // 请求设备
+    match device {
+        Ok(device) => {
+            console::log_1(&"Device requested successfully".into());
+            // 继续处理设备
+            let device: UsbDevice = device.unchecked_into();
+            web_sys::console::log_1(&format!("++++++++++++Product Name: {:?}", device.product_name()).into());
+            let open_promise = device.open();
+            JsFuture::from(open_promise).await.unwrap();
+
+            let configurations: Array = device.configurations();
+            let configuration: UsbConfiguration = configurations.get(0).unchecked_into();
+             // 获取接口数组
+             let interfaces: Array = configuration.interfaces();
+             // 获取第一个接口
+             let interface: UsbInterface = interfaces.get(0).unchecked_into();
+             let interface_number = interface.interface_number(); // 获取接口编号
+             // 选择配置，通常为默认配置 1
+            let select_configuration_promise = device.select_configuration(1);
+            JsFuture::from(select_configuration_promise).await.unwrap();
+             // 声明接口 
+            let claim_promise = device.claim_interface(interface_number);
+            wasm_bindgen_futures::JsFuture::from(claim_promise).await.unwrap();
+
+            let mut hid_device_obj = WEB_USB_DEVICE.lock();
+            *hid_device_obj = Some(UsbDeviceBox(device));
+            Ok(())
+        },
+        Err(err) => {
+            console::log_1(&format!("Error requesting device: {:?}", err).into());
+            Ok(())
+        }
+    }
+
+}
+
+#[cfg(target_arch = "wasm32")]
+// #[wasm_bindgen]
+pub async fn send_apdu(apdu: &str) -> Result<String> {
+
+    console::log_1(&"enter send_apdu function".into());
+    console::log_1(&format!("-->{:?}", apdu).into());
+    // 访问存储的 UsbDevice
+    let hid_device_obj = WEB_USB_DEVICE.lock();
+    let mut response_data = "".to_string();
+    if let Some(device) = &*hid_device_obj {
+        // 这里可以使用 device 进行操作
+        console::log_1(&format!("Using device: {:?}", device.0.product_name()).into());
+        response_data = send_and_receive(&device.0, hex::decode(apdu).unwrap().as_slice()).await;
+    } else {
+        console::log_1(&"No device found".into());
+    }
+    console::log_1(&format!("<--{:?}", response_data.clone()).into());
+
+    Ok(response_data)
 }
 
 
@@ -38,11 +120,9 @@ fn make_blocks(apdu: &[u8]) -> Vec<BytesMut> {
     let mut data = BytesMut::with_capacity(2 + apdu.len());//指令长度（2） + 指令原值
     data.put(as_u16_be(apdu.len()));
     data.put(apdu);
-    println!("data-->{}", hex::encode(data.clone()));
     let packet_size = 64;
     let block_size = packet_size - 5;
     let nb_blocks = (data.len() + block_size - 1) / block_size;
-    println!("nb_blocks-->{}", nb_blocks);
     let mut blocks: Vec<BytesMut> = Vec::with_capacity(nb_blocks);
     let mut data_index = 0;
 
@@ -228,24 +308,4 @@ pub async fn send_and_receive(device: &UsbDevice, apdu: &[u8]) -> String{
     }
 
     hex::encode(response_acc.data)
-}
-
-#[cfg(test)]
-mod test{
-    use super::make_blocks;
-
-    #[test]
-    fn test(){
-        let result = make_blocks(hex::decode("00A40400001111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111122").unwrap().as_slice());
-        let result = make_blocks(hex::decode("00A40400001111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111122222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222").unwrap().as_slice());
-        // let result = make_blocks(hex::decode("0000").unwrap().as_slice());
-        for val in result {
-            println!("-->{}", hex::encode(val));
-        }
-    }
-
-    #[test]
-    fn read_test(){
-        
-    }
 }
