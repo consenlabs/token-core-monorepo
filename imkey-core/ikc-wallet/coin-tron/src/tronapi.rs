cece9e91999d9d8d130dc612ad204504bd6da1b2
xiaoguang1010 <40228114+xiaoguang1010@users.noreply.github.com>
2024-10-25 14:51:21 +0800

    feat: tron sign message support v2 version[R2D2-11852] (#131)
    * feat: tron sign message support v2 version
    
    * chore: tron sign message v2 review problem optimization
    
    * chore: add proro field description

--- imkey-core/ikc-wallet/coin-tron/src/tronapi.rs
+++ imkey-core/ikc-wallet/coin-tron/src/tronapi.rs
@@ -13,12 +13,14 @@ pub struct TronTxOutput {
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct TronMessageInput {
-    #[prost(string, tag = "2")]
+    #[prost(string, tag = "1")]
     pub message: ::prost::alloc::string::String,
-    #[prost(bool, tag = "4")]
+    #[prost(bool, tag = "2")]
     pub is_hex: bool,
-    #[prost(bool, tag = "5")]
-    pub is_tron_header: bool,
+    #[prost(string, tag = "3")]
+    pub header: ::prost::alloc::string::String,
+    #[prost(uint32, tag = "4")]
+    pub version: u32,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
